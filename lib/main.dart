e8422628091e2fdadab752dc62967cb0859c0398
myid-hana <sosqkrgksml@gmail.com>
2024-11-29 15:09:30 +0900

    카카오 로그인 SDK 통합 및 iOS, Android 설정 업데이트

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:cute_hamong/config/router.dart';
 import 'package:cute_hamong/style/theme.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'firebase_options.dart';
 
 void main() {
@@ -11,6 +12,10 @@ void main() {
   runZonedGuarded(() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
+    );
+
+    KakaoSdk.init(
+      nativeAppKey: '361236e911593a4582642984d1cafbd9',
     );
   }, (_, stackTrace) {});
 
