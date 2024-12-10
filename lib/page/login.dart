3544aab3e95f703bc2352d1ffa3213f551a15102
myid-hana <sosqkrgksml@gmail.com>
2024-12-10 14:38:42 +0900

    구글 로그인 기능을 Firebase와 연동하여 구현

--- lib/page/login.dart
+++ lib/page/login.dart
@@ -1,11 +1,35 @@
 import 'package:cute_hamong/components/login_button.dart';
 import 'package:cute_hamong/style/color.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
 class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
+
+  Future<void> signInWithGoogle() async {
+    GoogleSignIn googleSignIn = GoogleSignIn();
+    GoogleSignInAccount? account = await googleSignIn.signIn();
+    if (account == null) {
+      print('로그인 실패');
+      return;
+    }
+
+    GoogleSignInAuthentication authentication = await account.authentication;
+    OAuthCredential googleCredential = GoogleAuthProvider.credential(
+      idToken: authentication.idToken,
+      accessToken: authentication.accessToken,
+    );
+    UserCredential credential =
+        await FirebaseAuth.instance.signInWithCredential(googleCredential);
+    if (credential.user == null) {
+      print('파이어베이스 연동 실패');
+      return;
+    }
+    final user = credential.user;
+    print('로그인 성공 ${user!.displayName}');
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,15 +71,7 @@ class LoginPage extends StatelessWidget {
               text: '구글로 로그인하기',
               image: 'lib/assets/images/google.png',
               backgroundColor: Colors.white,
-              onPressed: () async {
-                GoogleSignIn googleSignIn = GoogleSignIn();
-                GoogleSignInAccount? account = await googleSignIn.signIn();
-                if (account == null) {
-                  print('구글계정으로 로그인 실패');
-                  return;
-                }
-                print('구글계정으로 로그인 성공 ${account!.email}');
-              },
+              onPressed: () => signInWithGoogle(),
             ),
           ],
         ),
